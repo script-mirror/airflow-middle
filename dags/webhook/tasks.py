37cc81bbc3b476551ecdf7adeb478c97e189c012
raizen-arthurmoraes2 <arthur.moraes2@raizen.com>
2025-09-25 14:03:44 -0300

    refact: rodando script com docker operator para melhorar a visibilidade

--- dags/webhook/tasks.py
+++ dags/webhook/tasks.py
@@ -1,7 +1,5 @@
 from airflow.decorators import task
-import requests
 from middle.utils import (
-    get_auth_header,
     Constants,
     setup_logger,
     sanitize_string,
@@ -21,19 +19,3 @@ def start_task(**kwargs):
 @task
 def end_task(**kwargs):
     return None
-
-def executar_task(**kwargs):
-    params = kwargs.get('dag_run').conf
-    product_name = params.get('nome')
-    res = requests.post(
-        f"{constants.BASE_URL}/tasks/api/webhook",
-        headers=get_auth_header(),
-        json=params,
-    )
-    try:
-        res.raise_for_status()
-        logger.info(f"API chamada com sucesso para o produto {product_name}: {res.text}")
-    except Exception:
-        logger.error(f"Erro ao chamar API para o produto {product_name}: {res.text}")
-        raise Exception(f"Erro ao chamar API para o produto {product_name}: {res.text}")
-    return ["end_task"]
